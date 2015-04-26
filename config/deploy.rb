ed48b109f3542c5783edacb9d0e716e19c235351
everblut <everblut@gmail.com>
2015-04-26 17:22:41 -0500

    prepare deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,47 +2,53 @@
 lock '3.4.0'
 
 set :application, 'savepoint'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:everblut/savepoint.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Default value for :scm
+set :scm, :git
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# Default value for :format
+set :format, :pretty
 
-# Default value for :scm is :git
-# set :scm, :git
+# Default value for :log_level
+set :log_level, :debug
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Default value for :pty
+set :pty, true
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
+# Default value for :linked_files
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# Default value for linked_dirs
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# Default value for keep_releases
+set :keep_releases, 3
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
+set :puma_threads, [1,4]
+set :puma_workers, 1
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  task :precompile do
+     on roles(:app) do
+       within release_path do
+         with rails_env: fetch(:stage) do
+           execute :rake, "assets:precompile"
+         end
+       end
+     end
+   end
 
 end
